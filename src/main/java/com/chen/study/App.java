ee86f6f6a76812136e66db715a81ba9456e012db
chen <2664582204@qq.com>
2020-06-25 19:31:50 +0800

    环境测试

---
+++ src/main/java/com/chen/study/App.java
@@ -0,0 +1,11 @@
+package com.chen.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World! this is test git>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" );
+    }
+}
